f45ede2fbe0798aef479feb9857d3e81a8b4230a
jdmedlock <jdmedlock@gmail.com>
2020-06-26 11:23:30 -0500

    Feature: Setup repo
    Initial setup of our repo
    
    Resolves: N/a
    See also: N/a

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+module.exports = {
+  //
+  graphql: {
+    endpoint: '/graphql',
+    tracing: false,
+    shadowCRUD: true,
+    playgroundAlways: false,
+    depthLimit: 7,
+    amountLimit: 100,
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '95044e6f-9a5c-43e1-a7c2-1b4aa0ebf99a'
+};
